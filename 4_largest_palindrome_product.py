c6bdd8eb3ef699f7154790c1e45f61a00c3b1fd1
modlinski <modlinski@modlinski-GL72-6QC>
2017-08-02 17:57:23 +0200

    Remove old solutions.

--- 4_largest_palindrome_product.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
-for i in xrange(1, 1000):
-    for j in xrange(1, 1000):
-        num = i*j
-        for number in str(num):
-            print i*j
