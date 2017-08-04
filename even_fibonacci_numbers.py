0929ed66f12a712871d9176f7a3998c43877d250
Michal Modlinski (GL) <michal.modlinski@globallogic.com>
2017-08-04 13:05:58 +0200

    Remove unnecessary blank space.

--- even_fibonacci_numbers.py
+++ even_fibonacci_numbers.py
@@ -7,7 +7,6 @@
 """
 from time import time
 
-
 # The Fibonacci numbers are the numbers in the following integer sequence, called the Fibonacci sequence. By definition,
 # the first two numbers in the Fibonacci sequence are either 1 and 1, or 0 and 1, depending on the chosen starting point
 # of the sequence, and each subsequent number is the sum of the previous two. In this problem Fibonacci sequence is
