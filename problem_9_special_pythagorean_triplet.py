305549237293f03051a6f6efc8e3c78b9038def8
modlinski <modlinski@modlinski-GL72-6QC>
2017-08-20 23:25:17 +0200

    First not efficient solution.

--- problem_9_special_pythagorean_triplet.py
+++ problem_9_special_pythagorean_triplet.py
@@ -8,20 +8,17 @@
 from time import time
 
 
-def triplet():
+def triplet_1():
     count = 0
     for a in range(1, 1000):
         for b in range(a, 1000):
             c = (1000 - a - b)
             count += 1
             if a**2 + b**2 == c**2:
-                print(count)
                 return a * b * c
 
 if __name__ == "__main__":
 
     start = time()
-    # assert triplet(1000) ==
-    print(triplet())
+    assert triplet_1() == 31875000
     print("Time of execution for triplet: ", time() - start)
-    start = time()
