bf9ecb63c31f3346eba66f783375ea76dd3e1233
modlinski <modlinski@modlinski-GL72-6QC>
2017-08-21 22:39:09 +0200

    The most efficient solution.

--- problem_9_special_pythagorean_triplet.py
+++ problem_9_special_pythagorean_triplet.py
@@ -6,7 +6,7 @@
 :created: 19.08.2017
 """
 from time import time
-from math import floor
+from math import floor, sqrt
 
 
 def triplet_1(s):
@@ -28,6 +28,63 @@ def triplet_2(s):
             if a ** 2 + b ** 2 == c ** 2:
                 return a * b * c
 
+
+def triplet_3(s):
+    # Conditions:
+    # if greatest_common_divisor(a, b, c) == 1 then triplet a, b, c is called primitive
+    # triplet a, b, c is primitive only if gcd(a, b) = 1
+    # all primitive triplets (a, b, c) can be represented as:
+    # a = m  ** 2 − n ** 2
+    # b = 2 * m * n
+    # c = m ** 2 + n ** 2
+    # and:
+    # m > n > 0 -> a < b
+    # exactly one of m, n is even
+    # greatest_common_divisor(m, n) = 1
+    # from any Pythagorean triplet you get a primitive one by dividing out the greatest common divisor, so every
+    # Pythagorean triplet has a unique representation:
+    # a = (m ** 2 − n ** 2) * d
+    # b = 2 * m * n * d
+    # c = (m ** 2 + n ** 2) * d
+    # and:
+    # m > n > 0 -> a < b
+    # exactly one of m, n is even
+    # greatest_common_divisor(m, n) = 1
+    # greatest_common_divisor(a, b, c) = d
+    # Using that parametrisation:
+    # s = a + b + c = 2 * m * (m + n) * d
+    def gcd(x, y):
+        while y:
+            x, y = y, x % y
+        return x
+    result = []
+    s_2 = s/2
+    for m in range(2, floor(sqrt(s_2))):
+        if s_2 % m == 0:
+            s_m = s_2 / m
+            # reduce the search space by removing all factors 2
+            while s_m % 2 == 0:
+                s_m /= 2
+            if m % 2 == 1:
+                k = m + 2
+            else:
+                k = m + 1
+            while k < 2 * m and k <= s_m:
+                if s_m % k == 0 and gcd(k, m) == 1:
+                    d = s_2 / (k * m)
+                    n = k - m
+                    a = d * (m * m - n * n)
+                    b = 2 * d * m * n
+                    c = d * (m * m + n * n)
+                    result.append((a, b, c))
+                k += 2
+    # return result  # for cases where more than triplet is correct value
+    product = 1
+    for num in result[0]:
+        product *= num
+    return product
+
+
 if __name__ == "__main__":
     start = time()
     assert triplet_1(1000) == 31875000
@@ -35,3 +92,6 @@ if __name__ == "__main__":
     start = time()
     assert triplet_2(1000) == 31875000
     print("Time of execution for triplet_2: ", time() - start)
+    start = time()
+    assert triplet_3(1000) == 31875000
+    print("Time of execution for triplet_3: ", time() - start)
