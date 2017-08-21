d6458995003dba99c372f35bf154b9429051c354
Michal Modlinski (GL) <michal.modlinski@globallogic.com>
2017-08-21 11:35:10 +0200

    Another more efficient solution.

--- problem_9_special_pythagorean_triplet.py
+++ problem_9_special_pythagorean_triplet.py
@@ -6,19 +6,32 @@
 :created: 19.08.2017
 """
 from time import time
+from math import floor
 
 
-def triplet_1():
-    count = 0
-    for a in range(1, 1000):
-        for b in range(a, 1000):
-            c = (1000 - a - b)
-            count += 1
-            if a**2 + b**2 == c**2:
+def triplet_1(s):
+    for a in range(1, s):
+        for b in range(a, s):
+            c = (s - a - b)
+            if a ** 2 + b ** 2 == c ** 2:
+                return a * b * c
+
+
+def triplet_2(s):
+    # Conditions:
+    # a < b < c
+    # a =< (s − 3) / 3
+    # b < (s − a) / 2
+    for a in range(3, 1 + floor((s - 3) / 3)):
+        for b in range(a + 1, floor((s - 1 - a) / 2)):
+            c = (s - a - b)
+            if a ** 2 + b ** 2 == c ** 2:
                 return a * b * c
 
 if __name__ == "__main__":
-
     start = time()
-    assert triplet_1() == 31875000
-    print("Time of execution for triplet: ", time() - start)
+    assert triplet_1(1000) == 31875000
+    print("Time of execution for triplet_1: ", time() - start)
+    start = time()
+    assert triplet_2(1000) == 31875000
+    print("Time of execution for triplet_2: ", time() - start)
