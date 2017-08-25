b9c7eaf327e457f079d010c287b499b25fe5f472
Michal Modlinski (GL) <michal.modlinski@globallogic.com>
2017-08-25 15:22:07 +0200

    Beginning of first solution.

--- problem_10_summation_of_primes.py
+++ problem_10_summation_of_primes.py
@@ -9,10 +9,13 @@ from time import time
 
 
 def summation_1(ceiling):
-    pass
+    for num in range(6, ceiling + 1, 6):
+        print(num - 1)
+        print(num + 1)
 
 if __name__ == "__main__":
     start = time()
     # assert summation_1(2000000) ==
-    print(summation_1(2000000))
+    # print(summation_1(2000000))
+    print(summation_1(200))
     print("Time of execution for summation_1: ", time() - start)
