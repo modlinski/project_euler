97e35b5a868c911527b71cc6f12a08536b6d6563
Michal Modlinski (GL) <michal.modlinski@globallogic.com>
2017-08-25 14:59:27 +0200

    File without solutions.

---
+++ problem_10_summation_of_primes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+:brief: Module containing solutions of Project Euler Problem 10.
+:author: Michal Modlinski
+:created: 25.08.2017
+"""
+from time import time
+
+
+def summation_1(ceiling):
+    pass
+
+if __name__ == "__main__":
+    start = time()
+    # assert summation_1(2000000) ==
+    print(summation_1(2000000))
+    print("Time of execution for summation_1: ", time() - start)
