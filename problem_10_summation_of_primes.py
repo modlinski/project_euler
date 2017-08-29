f88ecd5ed9d0644c153fe0105a92a1eb6d240dc7
Michal Modlinski (GL) <michal.modlinski@globallogic.com>
2017-08-29 12:36:00 +0200

    First solution.

--- problem_10_summation_of_primes.py
+++ problem_10_summation_of_primes.py
@@ -37,13 +37,14 @@ def summation_1(ceiling):
                             else:
                                 return True
     sum_of_primes = 2 + 3
-    for num in range(6, ceiling + 1, 6):
-        candidate_1 = num - 1
-        candidate_2 = num + 1
-        if is_prime(candidate_1):
-            sum_of_primes += candidate_1
-        if is_prime(candidate_2):
-            sum_of_primes += candidate_2
+    num = 5
+    while num <= ceiling:
+        if is_prime(num):
+            sum_of_primes += num
+        num += 2
+        if num <= ceiling and is_prime(num):
+            sum_of_primes += num
+        num += 4
     return sum_of_primes
 
 if __name__ == "__main__":
